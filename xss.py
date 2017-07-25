e854e8bedb32ba85946a97259d2e027fb560cff2
tomas <tomasbortoli@gmail.com>
2017-07-25 23:07:40 +0200

    working to support POST requests

--- xss.py
+++ xss.py
@@ -13,18 +13,20 @@ class XSS:
     def fuzz(self):
         i=0
         while i<len(self.requests):
-            if int(self.requests[i]['requestId'])>self.reached_id:
-                self.reached_id=int(self.requests[i]['requestId'])
-                print self.requests[i]['url']+' '+self.requests[i]['method']
-                if self.requests[i]['method']=='GET':
-                    for param in parse_qs(urlparse(self.requests[i]['url']).query):
-                        self.test('GET',self.requests[i]['url'],param)
-                    #print s.get(requests[i]['url']).text.encode('utf-8')
-                elif self.requests[i]['method']=='POST':
-                    pass #print s.post(requests[i]['url'],requests=requests[i]['requestBody']).text.encode('utf-8')
-                i+=2    # 2 because we want to skip the element that contains the recorded headers
-            else:
-                break
+            self.reached_id=int(self.requests[i]['requestId'])
+            print self.requests[i]['url']+' '+self.requests[i]['method']
+            if self.requests[i]['method']=='GET':
+                for param in parse_qs(urlparse(self.requests[i]['url']).query):
+                    self.test('GET',self.requests[i]['url'],param)
+                #print s.get(requests[i]['url']).text.encode('utf-8')
+            elif self.requests[i]['method']=='POST':
+                if 'formData' in self.requests[i]['requestBody']:
+                    for param in self.requests[i]['requestBody']['formData']:
+                        #print param
+                        self.test('POST',self.requests[i]['url'],param,self.requests[i]['requestBody'])
+                        #print s.post(requests[i]['url'],requests=requests[i]['requestBody']).text.encode('utf-8')
+            i+=2    # 2 because we want to skip the element that contains the recorded headers
+
 
     @staticmethod
     def substParam(url,name,newValue):
@@ -41,27 +43,38 @@ class XSS:
     def catchUp(self, s):
         i=0
         #print "len: "+str(len(self.requests))
-        print self.reached_id
         while int(self.requests[i]['requestId'])<self.reached_id:
-            print str(int(self.requests[i]['requestId']))
             if self.requests[i]['method']=='GET':
-                pass #print s.get(self.requests[i]['url']).text.encode('utf-8')
+                response= s.get(self.requests[i]['url']).text.encode('utf-8')
+                XSS.verify(response)
             elif self.requests[i]['method']=='POST':
-                pass #print s.post(self.requests[i]['url'],requests=self.requests[i]['requestBody']).text.encode('utf-8')
+                response= s.post(self.requests[i]['url'],params=self.requests[i]['requestBody']).text.encode('utf-8')
+                XSS.verify(response)
             i+=2
             #print i
 
+    @staticmethod
+    def verify(response):
+        if XSS.inj1 in response:
+            print "XSS found !!"
 
-    def test(self, method, url, param):
+    def test(self, method, url, param,postData=None):
         s = requests.Session()
-        #if method=='GET':
+        if method=='GET':
             #res=s.get(url)
             #text=res.text.encode('utf-8')
-        newUrl = XSS.substParam(url,param,XSS.inj1)
-        self.catchUp(s)
-        s.get(newUrl).text.encode('utf-8')
+            newUrl = XSS.substParam(url,param,XSS.inj1)
+            self.catchUp(s)
+            response = s.get(newUrl).text.encode('utf-8')
+            XSS.verify(response)
 
-        print "##################"
-        print newUrl
-        print "##################"
-        print param
+            print "##################"
+            print newUrl
+            print "##################"
+            print param
+        elif method=='POST':
+            newPost=copy.deepcopy(postData)
+            newPost['formData'][param]=XSS.inj1
+            self.catchUp(s)
+            response = s.post(url,params=newPost).text.encode('utf-8')
+            XSS.verify(response)
